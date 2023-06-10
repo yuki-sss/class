a1eb6ab35a22acaf2c5ab6f334df6d750688ece4
SHIMADU YUKI <aileedelweiss@yahoo.co.jp>
2023-06-11 01:42:24 +0900

    完了

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("Hello World!");
+(() => {
+  class Accordion {
+    constructor(obj) {
+      console.log("obj", obj.tagName);
+      const $elm = document.querySelector(obj.hookName);
+      const $trigger = $elm.getElementsByTagName(obj.tagName);
+
+      const triggerLen = $trigger.length;
+      let index = 0;
+      while (index < triggerLen) {
+        $trigger[index].addEventListener("click", (e) => this.clickHandler(e));
+        index++;
+      }
+    }
+    clickHandler = (e) => {
+      e.preventDefault();
+      const $target = e.currentTarget;
+      const $content = $target.nextElementSibling;
+      if ($content.style.display === "block") {
+        $content.style.display = "none";
+      } else {
+        $content.style.display = "block";
+      }
+    };
+  }
+
+  const fuckingAccordion = new Accordion({
+    hookName: "#js-faq",
+    tagName: "p",
+  });
+  const dummyAccordion = new Accordion({
+    hookName: "#js-accordion",
+    tagName: "a",
+  });
+  const miniAccordion = new Accordion({
+    hookName: "#js-accordion-mini",
+    tagName: "dt",
+  });
+})();
